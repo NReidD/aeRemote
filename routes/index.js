ce79b7a74823ea73142d8733e16f2aae0aea5942
Nathan Reid <nathan.reid@salesianum.org>
2023-08-25 18:07:58 +0000

    html

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var net = require('net')
 
 /* GET home page. */
 var x = 0
@@ -7,8 +8,10 @@ while (x< 12)
 {
     x = x+ 5
 }
+var str = ""
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ae2', count: x});
+  res.render('index', { title: 'Ae2', count: str});
 });
 router.get('/data', function(req, res, next) {
   res.render('data', { title: 'DataSheet', count: x});
@@ -20,11 +23,13 @@ router.get('/cpu', function(req, res, next) {
   res.render('cpu', { title: 'CPU', count: x});
 });
 router.get('/msg', function(req, res, next) {
-  res.render('msg', { title: 'Messages', count: x});
+  res.render('msg', { title: 'Messages', count: str});
 });
 function Fetchr()
 {
 x = x*2   
 }
-setInterval(Fetchr, 500);
+
+console.log("hi")
+
 module.exports = router;
