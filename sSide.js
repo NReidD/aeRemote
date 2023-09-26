d3f0cde2893ac171a3645e78d0e1cbf017d35567
= <=>
2023-09-25 22:08:15 -0400

    o

---
+++ sSide.js
@@ -0,0 +1,21 @@
+var net = require('net')
+
+const server = net.createServer((c) => {
+    c.setEncoding('utf-8')
+    console.log('client connected');
+    x = 1
+    c.on('end', () => {
+        console.log('client disconnected');
+    });
+    c.setDefaultEncoding('utf-8')
+    c.on('data', (data) => {
+      console.log(data)
+datas = data
+    })
+    c.write('hello\r\n');
+    c.pipe(c);
+    module.exports = {server,x, c}
+});
+server.listen(9000, () => {
+ console.log("test")
+})
