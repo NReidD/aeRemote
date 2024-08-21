c71190456dfe60c92e891d6d4a1549ff5eb14ad9
NReidD <nathan.reid@salesianum.org>
2024-08-20 21:17:02 -0400

    WOOHOo

--- bin/servSide.js
+++ bin/servSide.js
@@ -1,31 +1,21 @@
-const net = require('net');
-const port = 9000;
+var net = require('net')
 
-const server = net.createServer();
-server.listen(port, () => {
-    console.log('TCP Server is running on port ' + port + '.');
-});
-
-let sockets = [];
-
-server.on('connection', function(sock) {
-    console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
-    sockets.push(sock);
-
-    sock.on('data', function(data) {
-        console.log('DATA ' + sock.remoteAddress + ': ' + data);
-        // Write the data back to all the connected, the client will receive it as data from the server
-        sockets.forEach(function(sock, index, array) {
-            sock.write(sock.remoteAddress + ':' + sock.remotePort + " said " + data + '\n');
-        });
+const server = net.createServer((c) => {
+    c.setEncoding('utf-8')
+    console.log('client connected');
+    x = 1
+    c.on('end', () => {
+        console.log('client disconnected');
     });
-
-    // Add a 'close' event handler to this instance of socket
-    sock.on('close', function(data) {
-        let index = sockets.findIndex(function(o) {
-            return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
-        })
-        if (index !== -1) sockets.splice(index, 1);
-        console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
-    });
+    c.setDefaultEncoding('utf-8')
+    c.on('data', (data) => {
+      console.log(data)
+datas = data
+    })
+    c.write('hello\r\n');
+    c.pipe(c);
+    module.exports = {server,x, c}
 });
+server.listen(9000, () => {
+ console.log("test")
+})
