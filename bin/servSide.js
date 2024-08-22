0d0cb941b45851afaeddb7d27defedefe365f2b2
NReidD <nathan.reid@salesianum.org>
2024-08-21 21:14:00 -0400

    hi

--- bin/servSide.js
+++ bin/servSide.js
@@ -20,6 +20,8 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello, World!\n');
   }, 1000);
+  console.log("hi");
+  
 });
 
 // Handle server errors
