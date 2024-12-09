f5a192a0ac98af54ed2217efa3825964caa80c6a
NReidD <nathan.reid@salesianum.org>
2024-12-09 00:08:07 -0500

    m

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,14 @@
-const express = require('express');
-const cors = require('cors');
+   const express = require('express');
+   const cors = require('cors');
 
-const app = express();
-app.use(cors());
+   const app = express();
+   app.use(cors());
 
+   app.get('/api/hello', (req, res) => {
+     res.json({ message: 'Hello from the backend!' });
+   });
 
-app.get('/api/hello', (req, res) => {
-    console.log('====================================');
-    console.log("HEEEE");
-    console.log('====================================');
-  res.json({ message: 'Hello from th!' });
-
-});
-
-const port = 2999;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+   const port = 2999;
+   app.listen(port, () => {
+     console.log(`Server listening on port ${port}`);
+   });
