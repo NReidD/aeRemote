1f9c5ad4ade77c23d3766422cad4db00f5e47adc
NReidD <nathan.reid@salesianum.org>
2024-12-08 23:36:45 -0500

    autism

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from the backend!' });
 });
 
-const port = 3000;
+const port = 2999;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
